6d6c0f443cada87fa4d615084bb67a71805e666b
abinasureskumar <abina30.08.2003@gmail.com>
2023-01-23 13:10:20 +0100

    Added Unit Test

--- app/src/androidTest/java/com/abi/flappybird/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/abi/flappybird/ExampleInstrumentedTest.java
@@ -1,10 +1,21 @@
 package com.abi.flappybird;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +28,20 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> senario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.abi.flappybird", appContext.getPackageName());
+    }
+
+    @Test
+    public void testPlayButton() {
+        Espresso.onView(ViewMatchers.withId(R.id.PlayButton))
+                .perform(ViewActions.click());
     }
 }
